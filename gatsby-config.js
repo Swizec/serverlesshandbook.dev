8d0d5550d5fb6a36cf93d8bd1b59b86eac454543
Swizec Teller <swizec@swizec.com>
2019-10-06 13:56:38 -0700

    Gatsby config and various dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,93 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `Serverless Handbook`,
+        description: `Hello 👋`,
+        author: `@swizec`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        'gatsby-plugin-styled-components',
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: `${__dirname}/content/chapters`,
+                name: 'chapters'
+            }
+        },
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                plugins: [{
+                        resolve: 'gatsby-remark-images',
+                        options: {
+                            showCaptions: true,
+                            withWebp: true
+                        }
+                    },
+                    {
+                        resolve: 'gatsby-remark-responsive-iframe',
+                        options: {
+                            wrapperStyle: 'margin-bottom: 1.0725rem',
+                        },
+                    },
+                    'gatsby-remark-prismjs',
+                    'gatsby-remark-copy-linked-files',
+                    'gatsby-remark-smartypants',
+                    {
+                        resolve: `@raae/gatsby-remark-oembed`,
+                        options: {
+                            // defaults to false
+                            // usePrefix: true,
+                            providers: {
+                                // Important to exclude providers
+                                // that adds js to the page.
+                                // If you do not need them.
+                                exclude: ['Reddit'],
+                            },
+                        },
+                    },
+                    'gatsby-remark-a11y-emoji',
+                    {
+                        resolve: 'gatsby-plugin-typography',
+                        options: {
+                            pathToConfigModule: 'src/typography',
+                        },
+                    },
+                ]
+            }
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `serverless-handbook`,
+                short_name: `serverless`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                trackingId: 'UA-1464315-32',
+                head: false,
+                anonymize: true,
+                respectDNT: true,
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
 }
