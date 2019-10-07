72861f29c12fdff585629a391f01b1f503a25cb5
Swizec Teller <swizec@swizec.com>
2019-10-07 05:54:06 -0700

    The scary blank page

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,21 +26,12 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built by
+        {` `}
+        <a href="https://www.swizec.com">Swizec</a> with ❤️
+      </footer>
     </>
   )
 }
