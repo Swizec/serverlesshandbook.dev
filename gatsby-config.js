5756fa1320942ca99149ba1ba3ba0447acb0b9bb
Swizec Teller <swizec@swizec.com>
2019-10-07 22:19:16 -0700

    Add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,32 @@
 const remarkPlugins = [
-  require('remark-slug'),
+    require('remark-slug'),
 ]
 
 module.exports = {
-  plugins: [
-    {
-      resolve: 'gatsby-plugin-mdx',
-      options: {
-        extensions: ['.mdx', '.md'],
-        remarkPlugins,
-      }
-    },
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-theme-ui',
-    'gatsby-plugin-react-helmet',
-  ],
+    plugins: [{
+            resolve: 'gatsby-plugin-mdx',
+            options: {
+                extensions: ['.mdx', '.md'],
+                remarkPlugins,
+            }
+        },
+        'gatsby-plugin-catch-links',
+        'gatsby-plugin-theme-ui',
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                trackingId: 'UA-1464315-32',
+                anonymize: true,
+                respectDNT: true
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-facebook-pixel',
+            options: {
+                pixelId: '2634718133254322'
+            }
+        },
+        'gatsby-plugin-simple-analytics'
+    ],
 }
