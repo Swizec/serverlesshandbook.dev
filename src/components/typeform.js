51dfe6c2d56244046921efd9898ba5819162769a
Swizec Teller <swizec@swizec.com>
2019-10-08 22:52:22 -0700

    Add thank you page

---
+++ src/components/typeform.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react"
+import { Box } from "rebass"
+
+const Typeform = () => {
+  useEffect(() => {
+    // Taken from typeform
+    ;(function() {
+      var qs,
+        js,
+        q,
+        s,
+        d = document,
+        gi = d.getElementById,
+        ce = d.createElement,
+        gt = d.getElementsByTagName,
+        id = "typef_orm",
+        b = "https://embed.typeform.com/"
+      if (!gi.call(d, id)) {
+        js = ce.call(d, "script")
+        js.id = id
+        js.src = b + "embed.js"
+        q = gt.call(d, "script")[0]
+        q.parentNode.insertBefore(js, q)
+      }
+    })()
+  }, [])
+
+  return (
+    <Box
+      className="typeform-widget"
+      data-url="https://swizecteller.typeform.com/to/AJgFM5"
+      data-transparency="50"
+      data-hide-headers="true"
+      data-hide-footer="true"
+      sx={{ height: 500 }}
+    ></Box>
+  )
+}
+
+export default Typeform
