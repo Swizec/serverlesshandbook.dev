a37e66a0d90d78e84a073515216d988db89a5c2e
Swizec Teller <swizec@swizec.com>
2019-10-16 22:54:21 -0700

    Fix image embeds

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,40 @@ const remarkPlugins = [
 
 module.exports = {
     plugins: [{
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: `${__dirname}/src/images`
+            }
+        },
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
+        {
             resolve: 'gatsby-plugin-mdx',
             options: {
                 extensions: ['.mdx', '.md'],
                 remarkPlugins,
+                plugins: ['gatsby-remark-images'],
+                gatsbyRemarkPlugins: [
+                    'gatsby-remark-copy-linked-files',
+                    {
+                        resolve: 'gatsby-remark-images',
+                        options: {
+                            markdownCaptions: true,
+                            maxWidth: 890,
+                            linkImagestoOriginal: false,
+                            showCaptions: ['title', 'alt'],
+                            withWebp: true,
+                            tracedSVG: {
+                                color: `lightgray`,
+                                optTolerance: 0.4,
+                                turdSize: 100,
+                                turnPolicy: 'TURNPOLICY_MAJORITY',
+                            },
+                        },
+                    },
+                    // 'gatsby-remark-autolink-headers',
+                ]
             }
         },
         'gatsby-plugin-catch-links',
