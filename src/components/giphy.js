8f1d31ebbda9c3b36e41109aa274a1d0f1d97c84
Swizec Teller <swizec@swizec.com>
2019-10-24 09:01:30 -0700

    Write pros&cons chapter

--- src/components/giphy.js
+++ src/components/giphy.js
@@ -3,16 +3,24 @@ import fetch from "isomorphic-fetch"
 
 const API_KEY = "BbjXTpBIYN0GwoBCRpPLUCF08EPJ6PUp"
 
-export const Giphy = ({ search }) => {
+export const Giphy = ({ search, id }) => {
   const [src, setSrc] = useState([])
 
   useEffect(() => {
     ;(async () => {
-      const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${search}&limit=1&sort=relevant&lang=en`
+      if (search) {
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${search}&limit=1&sort=relevant&lang=en`
 
-      const { data: gifs } = await fetch(url).then(res => res.json())
+        const { data: gifs } = await fetch(url).then(res => res.json())
 
-      setSrc([{ src: gifs[0].images.looping.mp4, type: "video/mp4" }])
+        setSrc([{ src: gifs[0].images.looping.mp4, type: "video/mp4" }])
+      } else if (id) {
+        const url = `https://api.giphy.com/v1/gifs/${id}?api_key=${API_KEY}`
+
+        const { data } = await fetch(url).then(res => res.json())
+
+        setSrc([{ src: data.images.looping.mp4, type: "video/mp4" }])
+      }
     })()
   }, [search])
 
