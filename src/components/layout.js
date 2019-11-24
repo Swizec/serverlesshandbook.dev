2f45f09a0c01f2dd04f143a5637d9ae7d9059fa7
Felipe César <felipe.cesar00@gmail.com>
2019-11-24 07:28:41 +0700

    quick fix paywall in empty pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -57,7 +57,7 @@ const Paywall = ({ floating }) => {
           position: absolute;
           top: ${Math.round(dimensions.height * 0.2)}px;
           width: ${Math.round(dimensions.width)}px;
-          ${typeof document !== 'undefined' && document.body.scrollHeight < 6000 && 'display: none' }
+          ${typeof document !== 'undefined' && document.body.scrollHeight < 5000 && 'display: none' }
       `
       })
     }
