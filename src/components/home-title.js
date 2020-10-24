989c6a9b282acff3e5ea672ddf52f0b9d95df2db
Swizec Teller <swizec@swizec.com>
2020-10-24 07:36:36 -0700

    Try gumroad button fix

--- src/components/home-title.js
+++ src/components/home-title.js
@@ -1,49 +1,51 @@
 import React from "react"
 import { Box, Flex } from "rebass"
 import GumroadButton from "./GumroadButton"
-import * as coverImg from '../images/cover.svg'
+import * as coverImg from "../images/cover.svg"
 
 const HomeTitle = () => (
- <Flex flexWrap='wrap' sx={{textAlign: 'center', margin: 'auto auto'}}>
-  <Box
-    p={3}
-    width={1/2}
-    sx={{
-      minWidth: '250px',
-      margin: 'auto auto'
-    }}
-  >
-    <Box sx={{ textAlign: "center", }} >
-      <h1 style={{fontSize: '2em'}}>
-        Serverless Handbook <br /><small style={{fontSize: '0.5em'}}>for frontend engineers</small>
-      </h1>
-      Secretly this is going to be a resource teaching frontend engineers everything they need to know to dive into backend
+  <Flex flexWrap="wrap" sx={{ textAlign: "center", margin: "auto auto" }}>
+    <Box
+      p={3}
+      width={1 / 2}
+      sx={{
+        minWidth: "250px",
+        margin: "auto auto",
+      }}
+    >
+      <Box sx={{ textAlign: "center" }}>
+        <h1 style={{ fontSize: "2em" }}>
+          Serverless Handbook <br />
+          <small style={{ fontSize: "0.5em" }}>for frontend engineers</small>
+        </h1>
+        Secretly this is going to be a resource teaching frontend engineers
+        everything they need to know to dive into backend
+      </Box>
+
+      <Box sx={{ marginTop: "10px" }}>
+        <GumroadButton>
+          <a
+            className="gumroad-button"
+            href="https://gum.co/qdNn?wanted=true"
+            data-gumroad-single-product="true"
+            target="_blank"
+            rel="noopener noreferrer"
+          >{`start learning for $0+`}</a>
+        </GumroadButton>
+      </Box>
     </Box>
 
-    <Box sx={{marginTop: '10px'}}>
-      <GumroadButton  >
-        <a
-          className="gumroad-button"
-          href="https://gum.co/qdNn?wanted=true"
-          data-gumroad-single-product="true"
-          target="_blank"
-          rel="noopener noreferrer"
-        >{`start learning for $0+`}</a>
-      </GumroadButton>
+    <Box
+      p={3}
+      width={1 / 2}
+      sx={{
+        minWidth: "250px",
+        margin: "auto auto",
+      }}
+    >
+      <img src={coverImg} alt="Serverless Handbook Cover" />
     </Box>
-  </Box>
-
-  <Box
-    p={3}
-    width={1/2}
-    sx={{
-      minWidth: '250px',
-      margin: 'auto auto'
-    }}
-  >
-    <img src={coverImg} alt="Serverless Handbook Cover" />
-  </Box>
-</Flex>
+  </Flex>
 )
 
 export default HomeTitle
