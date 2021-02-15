760ff7596d503e9d4fb232df34599c20ef0217d9
Swizec Teller <swizec@swizec.com>
2021-02-15 07:07:26 -0800

    Create users, validate credentials

--- examples/serverless-auth-example/src/auth.ts
+++ examples/serverless-auth-example/src/auth.ts
@@ -1,9 +1,19 @@
 import { APIGatewayEvent } from "aws-lambda"
 import * as db from "simple-dynamodb"
 import sha256 from "crypto-js/sha256"
+import omit from "lodash.omit"
 
 function response(statusCode: number, body: any) {
-  return { statusCode, body: JSON.stringify(body) }
+  return {
+    statusCode,
+    // permissive CORS headers
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+    },
+    body: JSON.stringify(body),
+  }
 }
 
 // Hashing your password before saving is critical
@@ -13,6 +23,21 @@ function hashPassword(username: string, password: string) {
   return sha256(
     `${password}${process.env.SALT}${username}${password}`
   ).toString()
+}
+
+async function createUser(username: string, password: string) {
+  const result = await db.updateItem({
+    TableName: process.env.USER_TABLE!,
+    Key: {
+      username,
+    },
+    UpdateExpression: `SET password = :password, createdAt = :createdAt`,
+    ExpressionAttributeValues: {
+      ":password": hashPassword(username, password),
+      ":createdAt": new Date().toISOString(),
+    },
+  })
+  return result.Attributes
 }
 
 // Logs you in based on username/password combo
@@ -27,30 +52,30 @@ export const login = async (event: APIGatewayEvent) => {
     })
   }
 
-  // username is the key, which means it must be unique
-  let user = await db.getItem({
+  // find user in database
+  let { Item: user } = await db.getItem({
     TableName: process.env.USER_TABLE!,
     Key: {
+      // username is the key, which means it must be unique
       username,
     },
   })
 
   if (!user) {
     // user was not found, create
-    user = await db.updateItem({
-      TableName: process.env.USER_TABLE!,
-      Key: {
-        username,
-      },
-      UpdateExpression: `SET password = :password, createdAt = :createdAt`,
-      ExpressionAttributeValues: {
-        ":password": hashPassword(username, password),
-        ":createdAt": new Date().toISOString(),
-      },
-    })
+    user = await createUser(username, password)
   } else {
     // check credentials
+    if (hashPassword(username, password) !== user.password) {
+      // 🚨
+      return response(401, {
+        status: "error",
+        error: "Bad username/password combination",
+      })
+    }
   }
 
-  return response(200, user)
+  // user was created or has valid credentials
+
+  return response(200, omit(user, "password"))
 }
