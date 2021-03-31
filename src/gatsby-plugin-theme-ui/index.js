58eff38761e1fa8092afe6a1fdfd7aa4f5ca4f00
Swizec Teller <swizec@swizec.com>
2021-03-31 06:25:52 -0700

    Update paywall copy

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,11 +2,30 @@ import { future } from "@theme-ui/presets"
 import merge from "lodash.merge"
 import { toTheme } from "@theme-ui/typography"
 import typography from "typography-theme-stow-lake"
+import amazonBuy from "../images/buy-now-amazon.png"
 import "typeface-neuton"
 import "typeface-lato"
 
 import { courseTheme } from "@swizec/gatsby-theme-course-platform"
 
-const theme = merge(future, toTheme(typography), courseTheme)
+const theme = merge(
+  future,
+  {
+    buttons: {
+      buy: {
+        cursor: "pointer",
+        fontWeight: "heading",
+        background: `url(${amazonBuy})`,
+        backgroundSize: "fit",
+        backgroundPosition: "center center",
+        backgroundRepeat: "no-repeat",
+        textIndent: "-1000%",
+        width: 240,
+      },
+    },
+  },
+  toTheme(typography),
+  courseTheme
+)
 
 export default theme
